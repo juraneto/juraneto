a13ca5894bc6d4b4c7512a194a8b5698d31d5833
juraneto <104909936+juraneto@users.noreply.github.com>
2022-07-10 14:34:24 -0300

    Update repositorio_cliente.py

--- repositorio_cliente.py
+++ repositorio_cliente.py
@@ -12,9 +12,22 @@ class RepositorioCliente:
             print('cliente cadastrado')
         else:
             print('cliente ja existe')
-
     def buscar(self, cpf):
         for cliente in self.clientes:
             if cliente.getCpf() == cpf:
                 return cliente
         return None
+    def atualizar(self, cliente: Cliente):
+            if self.buscar(cliente.get_Cpf()) is None:
+                print("Cliente não se encontra")
+            else:
+                cliente.set_Nome(cliente.get_Nome())
+                cliente.set_Endereco(cliente.get_Endereco())
+    def deletar(self, cpf: str):
+            if self.buscar(cpf) is None:
+                print("Cliente não se encontra")
+            else:
+                self._clientes.pop()
+                print("Cliente apagado")
+    def listar(self):
+          return self._clientes
